ac84b2c4ef1a48a98e70b9ae828ab17e3ee36c33
ROHIT CHAND <100352296+rohitchand3141@users.noreply.github.com>
2023-03-29 10:50:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,15 +10,23 @@ const lookup = {
   'Y': 'L','Z': 'M', '?': '?', ',': ','
 };
 
-function rot13(encodedStr){
-   let decodedArr = []; // Your Result goes here
-  // Only change code below this line
-
-  return ;//return decodedArr
+function rot13(str) {
+var decoded = '';
+for (var i = 0; i < str.length; i++) {
+var ascii = str[i].charCodeAt();
+if (ascii >= 65 && ascii <= 90) {
+decoded += String.fromCharCode((ascii - 65 + 13) % 26 + 65);
+} else if (ascii >= 97 && ascii <= 122) {
+decoded += String.fromCharCode((ascii - 97 + 13) % 26 + 97);
+} else {
+decoded += str[i];
+}
+}
+return decoded;
 }
 
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
+// Testing the function
+console.log(rot13("EBG13 rknzcyr.")); // "ROT13 example."
+console.log(rot13("Guvf fragrapr vf sha!")); // "This sentence is fun!"
 
 module.exports = rot13;
